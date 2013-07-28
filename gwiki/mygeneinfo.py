59f475a7cd6c2cc82c24fc9ad2c20859c65d49d6
Chinmay Naik <chin.naik26@gmail.com>
2013-07-29 03:43:39 +0530

    construction of items from mygeneinfo + wikidata items

--- gwiki/mygeneinfo.py
+++ gwiki/mygeneinfo.py
@@ -1,6 +1,8 @@
 
  
 import json,urllib2,re,urllib
+import wikidata
+import time
 try:
     import settings
 except ImportError as e:
@@ -12,6 +14,8 @@ import rcsb
 BASE_URL =  settings.mygene_base
 META_URL = settings.mygene_meta
 UNIP_URL = settings.uniprot_url
+
+MOUSE_TAXON_ID = 10090
 
 def getJson(url):
     ufile = None
@@ -41,31 +45,6 @@ def get(json,key):
     return result
     
     
-#Construct for human gene 
-def parse_HumanGene_json(gene_json):
-#edit: Refseq is a multivalue field
-    HGItem = HumanGene()
-    root = gene_json
-    
-    HGItem.setField("species", "Q5")
-    HGItem.setField("subclass of","Q7187")
-    name = get(root, 'name')
-    if re.match(r'\w', name):
-        name = name[0].capitalize()+name[1:]
-    HGItem.setField("Name", name)
-    entrez = get(root, 'entrezgene')
-    HGItem.setField("Entrez Gene ID", entrez)
-    HGItem.setField("Homologene ID", get(get(root, 'homologene'), 'id'))
-    HGItem.setField("gene symbol", get(root, 'symbol'))
-    HGItem.setField("Ensembl ID", get(get(root, 'ensembl'), 'gene'))
-    HGItem.setField("GenLoc_chr", get(get(root, 'genomic_pos'), 'chr'))
-    HGItem.setField("GenLoc_start", get(get(root, 'genomic_pos'), 'start'))
-    HGItem.setField("GenLoc_end", get(get(root, 'genomic_pos'), 'end'))
-    HGItem.setField("RefSeq",get(get(root, 'refseq'), 'rna'))
-    HGItem.setField("AltSymbols", get(root, 'alias'))
-    HGItem.setField("RNA ID",get(get(root, 'accession'), 'rna'))
-    
-    return HGItem
 
 def _queryUniprot(entrez):
     return uniprotAccForEntrezId(entrez)
@@ -180,18 +159,233 @@ def findReviewedUniprotEntry2(entries, entrez):
         if canonical: return canonical
         else: return entry
 
-def parse_HumanGene(entrez):
-    gene_json = getJson( BASE_URL + entrez )
-    return parse_HumanGene_json(gene_json)
+#To-do Gene Atlas image
+#Construct for human gene 
+def parse_HumanGene_json(gene_json,homolog_json):
+#edit: Refseq is a multivalue field
+    HGItem = HumanGene()
+    root = gene_json
+    
+    HGItem.setField("found in taxon", "Q5")
+    HGItem.setField("description","Human Gene")
+    HGItem.setField("subclass of","Q7187")
 
+    #for genes label = HGNC symbol
+    HGItem.setField("Name", get(root, 'symbol'))
+    entrez = get(root, 'entrezgene')
+    HGItem.setField("Entrez Gene ID", entrez)
+    HGItem.setField("Homologene ID", get(get(root, 'homologene'), 'id'))
+    HGItem.setField("gene symbol", get(root, 'symbol'))
+    HGItem.setField("Ensembl Gene ID", get(get(root, 'ensembl'), 'gene'))
+    
+    HGItem.setField("Ensembl Transcript ID", get(get(root, 'ensembl'), 'transcript'))
+    HGItem.setField("GenLoc_chr", get(get(root, 'genomic_pos'), 'chr'))
+    HGItem.setField("GenLoc_start", get(get(root, 'genomic_pos'), 'start'))
+    HGItem.setField("GenLoc_end", get(get(root, 'genomic_pos'), 'end'))
+    HGItem.setField("RefSeq",get(get(root, 'refseq'), 'rna'))
+    HGItem.setField("AltSymbols", get(root, 'alias'))
+    HGItem.setField("RefSeq RNA ID",get(get(root, 'accession'), 'rna'))
+    HGItem.setField("HGNC ID", get(root, 'HGNC'))
+    HGItem.setField("OMIM ID",get(root,'MIM'))
+    
+    #encodes  -- search for human protein
+    key = get(root,'name') 
+    ID = []
+    res = wikidata.search_Item(key)
+    #search for human protein, property = uniprot ID 
+    #most surely human protein is present, but still....
+    uniprot = findReviewedUniprotEntry(get(root, 'uniprot'), entrez)
+    if res:
+        uniprotID = 'p352'
+        ID = wikidata.search_claim(res,uniprotID ,uniprot)
+    #search result is null or corresponding human protein doesnot exist
+    if not ID:
+        #create human protein item
+        ID = wikidata.create_Item(key)
+        #add uniprot claim to human protein item
+        wikidata.addClaim(ID, 'p352',uniprot)    
+    HGItem.setField("encodes", ID)
+    
+    #ortholog
+    key = get(homolog_json,'symbol')
+    ID = []
+    res = wikidata.search_Item(key)
+    #search for mouse gene, property = entrez ID 
+    if res:
+        entrezID = 'p351'
+        mouse_entrez = get(homolog_json,'entrezgene')
+        ID = wikidata.search_claim(res,entrezID ,mouse_entrez)
+    #search result is null or corresponding mouse gene doesnot exist
+    if not ID:
+        #create mouse gene item
+        ID = wikidata.create_Item(key)
+        #add entrez claim to mouse gene item
+        mouse_entrez = get(homolog_json,'entrezgene')
+        wikidata.addClaim(ID, 'p3521',mouse_entrez)    
+    HGItem.setField("ortholog", ID)
+    
+    
+    
+    
+    return HGItem
+
+
+
+#Construct for mouse gene 
+def parse_MouseGene_json(homolog_json,gene_json):
+#edit: Refseq is a multivalue field
+    MGItem = MouseGene()
+    root = homolog_json
+    
+    MGItem.setField("found in taxon", "Q83310")
+    MGItem.setField("subclass of","Q7187")
+    MGItem.setField("description","Mouse Gene")
+    
+    #for mouse gene label = MGI symbol
+    MGItem.setField("Name",  get(root, 'symbol'))
+    entrez = get(root, 'entrezgene')
+    MGItem.setField("Entrez Gene ID", entrez)
+    MGItem.setField("Homologene ID", get(get(root, 'homologene'), 'id'))
+    MGItem.setField("gene symbol", get(root, 'symbol'))
+    MGItem.setField("Ensembl Gene ID", get(get(root, 'ensembl'), 'gene'))
+    MGItem.setField("Ensembl Transcript ID", get(get(root, 'ensembl'), 'transcript'))
+    MGItem.setField("GenLoc_chr", get(get(root, 'genomic_pos'), 'chr'))
+    MGItem.setField("GenLoc_start", get(get(root, 'genomic_pos'), 'start'))
+    MGItem.setField("GenLoc_end", get(get(root, 'genomic_pos'), 'end'))
+    MGItem.setField("RefSeq",get(get(root, 'refseq'), 'rna'))
+    MGItem.setField("AltSymbols", get(root, 'alias'))
+    MGItem.setField("RNA ID",get(get(root, 'accession'), 'rna'))
+    
+    #encodes  -- search for mouse protein
+    key = get(root,'name') 
+    ID = []
+    res = wikidata.search_Item(key)
+    #search for mouse protein, property = uniprot ID 
+    #most surely mouse protein is present, but still....
+    uniprot = findReviewedUniprotEntry(get(root, 'uniprot'), entrez)
+    if res:
+        uniprotID = 'p352'
+        ID = wikidata.search_claim(res,uniprotID ,uniprot)
+    #search result is null or corresponding human gene doesnot exist
+    if not ID:
+        #create mouse protein item
+        ID = wikidata.create_Item(key)
+        #add uniprot claim to mouse protein item
+        wikidata.addClaim(ID, 'p352',uniprot)    
+    MGItem.setField("encodes", ID)
+    
+    #ortholog
+    key = get(gene_json,'symbol')
+    ID = []
+    res = wikidata.search_Item(key)
+    #search for human gene, property = entrez ID 
+    if res:
+        entrezID = 'p351'
+        ID = wikidata.search_claim(res,entrezID ,get(gene_json,'entrezgene'))
+    #search result is null or corresponding human gene doesnot exist
+    if not ID:
+        #create human gene item
+        ID = wikidata.create_Item(key)
+        #add entrez claim to human gene item
+        human_entrez = get(gene_json,'entrezgene')
+        wikidata.addClaim(ID, 'p3521',human_entrez)    
+    MGItem.setField("encodes", ID)
+    
+    return MGItem
+
+#TO-do ec classification
+def parse_MouseProtein_json(Homolog_json):
+    root = Homolog_json
+    MPItem = HumanProtein()
+    
+    #found in taxon wikidata item mouse=Q83310 , protein=Q8054
+    MPItem.setField("Name", get(root,'name'))
+    MPItem.setField("description", "Mouse Protein")
+    MPItem.setField("found in taxon", "Q83310")
+    MPItem.setField("subclass of","Q8054")
+    name = get(root, 'name')
+    MPItem.setField("Name", name)
+    
+    entrez = get(root, 'entrezgene')
+    uniprot = findReviewedUniprotEntry(get(root, 'uniprot'), entrez)
+    MPItem.setField("Uniprot ID", uniprot)
+    
+    #MPItem.setField("EC number", get(root, 'ec'))
+    MPItem.setField("RefSeq Protein ID",get(get(root, 'refseq'), 'protein'))
+    MPItem.setField("Ensembl Protein ID", get(get(root, 'ensembl'), 'protein'))
+    
+    #GO TERMS
+    #Wikidata items for GO terms
+    GO_ID = 'p686'
+    if get(root, 'go'):
+        GO_DICT = get(root,'go')
+        for key in GO_DICT:
+            res_list = GO_DICT[key]
+            ID = []
+            for val in res_list:
+                #search for the item
+                title = val['term']
+                res = wikidata.search_Item(title)
+                GID = []
+                #search for the corresponding go term
+                if res:
+                    #fix val['id']='GO:223'  remove the first three elements 
+                    GID = wikidata.search_claim(res, GO_ID, val['id'][3:])
+                    
+                #Create GO Item if it does not exist
+                if not GID:
+                    GID = wikidata.create_Item(title)
+                    #add created id's for the go terms 
+                    
+                    wikidata.addClaim(GID, GO_ID, val['id'][3:])
+                ID.append(GID)
+            if key == 'CC':
+                MPItem.setField("cell component",ID)
+            if key == 'MF':
+                MPItem.setField("molecular function",ID)
+            if key == 'BP':
+                MPItem.setField("biological process",ID)
+    
+    #if get(root, 'go'):
+    #    MPItem.setField("cell component", parse_go_category(get(root['go'], 'CC')))
+    #    MPItem.setField("molecular function", parse_go_category(get(root['go'], 'MF')))
+    #    MPItem.setField("biological process", parse_go_category(get(root['go'], 'BP')))
+    
+    #PDB  - CHECK what if Human proteins donot have pdb Id?
+    pdbs = rcsb.pdbs_for_uniprot(uniprot)
+    if not pdbs:
+        pdbs = get(root, 'pdb')
+    MPItem.setField("PDB", pdbs)
+    
+    #For "encoded by" property search for corresponding gene item. If not present ,create it and obtain wikidata identifier
+    #search_title = HGNC symbol
+    key = get(root,'symbol') 
+    ID = []
+    res = wikidata.search_Item(key)
+    #search for mouse gene, property = entrez 
+    if res:
+        entrezID = 'p351'
+        ID = wikidata.search_claim(res,entrezID ,entrez)
+    #search result is null or corresponding mouse gene doesnot exist
+    if not ID:
+        #create mouse gene item
+        ID = wikidata.create_Item(key)
+        #add entrez claim to mouse gene item
+        wikidata.addClaim(ID, 'p351',entrez)    
+    MPItem.setField("encoded by", ID)
+    
+    return MPItem
+
+#TO-DO EC classification
 def parse_HumanProtein_json(gene_json):
     root = gene_json
     HPItem = HumanProtein()
     
-    #species wikidata item human=Q5 , protein=Q8054
+    #found in taxon wikidata item human=Q5 , protein=Q8054
+    #for proteins label= HGNC fullname
     HPItem.setField("Name", get(root,'name'))
     HPItem.setField("description", "Human Protein")
-    HPItem.setField("species", "Q5")
+    HPItem.setField("found in taxon", "Q5")
     HPItem.setField("subclass of","Q8054")
     name = get(root, 'name')
     HPItem.setField("Name", name)
@@ -200,40 +394,132 @@ def parse_HumanProtein_json(gene_json):
     uniprot = findReviewedUniprotEntry(get(root, 'uniprot'), entrez)
     HPItem.setField("Uniprot ID", uniprot)
     
-    HPItem.setField("EC number", get(root, 'ec'))
+   # HPItem.setField("EC number", get(root, 'ec'))
+    HPItem.setField("RefSeq Protein ID",get(get(root, 'refseq'), 'protein'))
+    HPItem.setField("Ensembl Protein ID", get(get(root, 'ensembl'), 'protein'))
     
-    #GO TERMS
-    
+    #Wikidata items for GO terms
+    GO_ID = 'p686'
     if get(root, 'go'):
-        HPItem.setField("cell component", parse_go_category(get(root['go'], 'CC')))
-        HPItem.setField("molecular function", parse_go_category(get(root['go'], 'MF')))
-        HPItem.setField("biological process", parse_go_category(get(root['go'], 'BP')))
+        GO_DICT = get(root,'go')
+        for key in GO_DICT:
+            res_list = GO_DICT[key]
+            ID = []
+            for val in res_list:
+                #search for the item
+                title = val['term']
+                # title has multiple words seperated by /   
+                # found
+                if title.find('/') != -1:
+                    match = re.search(r'([\w ]*)\/.*',title)
+                    title = match.group(1)
+                res = wikidata.search_Item(title)
+                GID = []
+                #search for the corresponding go term
+                if res:
+                    #fix val['id']='GO:223'  remove the first three elements 
+                    GID = wikidata.search_claim(res, GO_ID, val['id'][3:])
+                    
+                #Create GO Item if it does not exist
+                if not GID:
+                    GID = wikidata.create_Item(title)
+                    #add created id's for the go terms
+                    
+                    wikidata.addClaim(GID, GO_ID, val['id'][3:])
+                ID.append(GID)
+            if key == 'CC':
+                HPItem.setField("cell component",ID)
+            if key == 'MF':
+                HPItem.setField("molecular function",ID)
+            if key == 'BP':
+                HPItem.setField("biological process",ID)
     
-    #PDB  - CHECK what if Human proteins donot have pdb Id?
+    #PDB
     pdbs = rcsb.pdbs_for_uniprot(uniprot)
     if not pdbs:
-        pdbs = get(root, 'pdb')
-    #checmical_structure
-    
-        
-        
+        pdbs = get(root, 'pdb')   
     HPItem.setField("PDB", pdbs)
+    
+    #For "encoded by" property search for corresponding gene item. If not present ,create it and obtain wikidata identifier
+    #search_title = HGNC symbol
+    key = get(root,'symbol') 
+    ID = []
+    res = wikidata.search_Item(key)
+    #search for human gene, property = entrez 
+    if res:
+        entrezID = 'p351'
+        ID = wikidata.search_claim(res,entrezID ,entrez)
+    #search result is null or corresponding human gene doesnot exist
+    if not ID:
+        #create human gene item
+        ID = wikidata.create_Item(key)
+        #add entrez claim to human gene item
+        wikidata.addClaim(ID, 'p351',str(entrez))    
+    HPItem.setField("encoded by", ID)
+
     
     return HPItem
    
-    
-    
-    
-def parse_HumanProtein(entrez):
+
+
+def get_homolog(gene, taxon, json):
+    """Returns the homologous gene for a given gene in a given taxon.
+
+    Arguments:
+    - `gene`:  the original gene
+    - `taxon`: the taxon of the species in which to find a homolog
+    - `json`:  the mygene.info json document for original gene
+    """
+    homologs = get(get(json, 'homologene'), 'genes')
+    # isolate our particular taxon (returns [[taxon, gene]])
+    if homologs:
+        pair  = filter(lambda x: x[0]==taxon, homologs)
+        if pair:
+            return pair[0][1]
+        else: return None
+    else: return None
+
+def get_json_documents(entrez):
+    """Returns the three JSON documents needed to construct a ProteinBox.
+    Dict structure: {gene:json, meta:json, homolog:json}
+    For use as a helper method for the parse_json() method.
+
+    Arguments:
+    - `entrez`: human gene entrez id
+    """
     gene_json = getJson( BASE_URL + entrez )
-    return parse_HumanProtein_json(gene_json)
+    meta_json = getJson( META_URL )
+
+    homolog = get_homolog(entrez, MOUSE_TAXON_ID, gene_json)
+    homolog_json = getJson(BASE_URL+str(homolog)) if homolog else None
+
+    return {'gene_json':gene_json, 'meta_json':meta_json, 'homolog_json':homolog_json}
+
+
+def parse_json(gene_json, meta_json, homolog_json):
+    '''construct human/mouse gene/protein'''
+    HP = parse_HumanProtein_json(gene_json)
+    
+    HG = parse_HumanGene_json(gene_json,homolog_json)
+    
+    MP = parse_MouseProtein_json(gene_json)   
+    
+    MG = parse_MouseGene_json(homolog_json,gene_json)
+    
+    return HG,HP,MG,MP
     
     
+    
+    
+def Parse(entrez):
+    
+    return parse_json(**get_json_documents(entrez))
+
     
     
   
 if __name__ == '__main__':
-   parse_HumanProtein('5649')
+   Parse('5649')
 
  
   
